f6f2d5f65b9f8a6d324a583af46c981792d441a6
Aleksandra Trimailova <132407917+OuuAlex@users.noreply.github.com>
2023-08-30 11:29:13 +0200

    'update'

---
+++ src/test/java/org/ait/phonebook/HomePageTests.java
@@ -0,0 +1,17 @@
+package org.ait.phonebook;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTests extends TestBase{
+
+    @Test
+    public void isHomeComponentPresentTest(){
+        //driver.findElement(By.xpath("//h1[text()='Home Component']"));
+        isHomeComponentPresent();
+        //System.out.println("Home Component is " + isElementPresent(By
+        //        .xpath("//h1[text()='Home Component']")));
+        Assert.assertTrue(isHomeComponentPresent());
+    }
+
+}
