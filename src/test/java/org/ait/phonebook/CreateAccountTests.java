03020fd777dae9026a46b43bc8e0e4d0bd6853c2
Aleksandra Trimailova <132407917+OuuAlex@users.noreply.github.com>
2023-12-04 12:32:55 +0100

    update

--- src/test/java/org/ait/phonebook/CreateAccountTests.java
+++ src/test/java/org/ait/phonebook/CreateAccountTests.java
@@ -1,6 +1,7 @@
 package org.ait.phonebook;
 
-import org.openqa.selenium.By;
+
+import org.ait.phonebook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,26 +10,22 @@ public class CreateAccountTests extends TestBase {
     @BeforeMethod
     public void ensurePrecondition() {
         //precondition: user should be logged out
-        if (!isElementPresent(By.cssSelector("a:nth-child(4)"))) {
-            driver.findElement(By.xpath("//button[contains(.,'Sign Out')]")).click();
+        if (!app.getUser().isLoginLinkPresent()) {
+            app.getUser().clickOnSingOutButton();
         }
-        //click on Login link - a:nth-child(4) - css
-        driver.findElement(By.cssSelector("a:nth-child(4)")).click();
+        app.getUser().login();
+        app.getUser().clickOnLoginLink();
+        app.getUser().clickOnSingOutButton();
     }
 
     @Test
-    public void newUserRegistrationPositiveTest() {
+    public void existedUserRegistrationNegativeTest() {
         //enter email - [placeholder='Email'] - css
-        driver.findElement(By.cssSelector("[placeholder='Email']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).sendKeys("ouu@gmail.com");
-        //enter password - [placeholder='Password'] - css
-        driver.findElement(By.cssSelector("[placeholder='Password']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).sendKeys("Ouu123456$");
+        app.getUser().fillLoginRegistrationForm(new User().setEmail("ouu@gmail.com").setPassword("Ouu123456$"));
         //click on Registration button - //button[text()='Registration'] - xpath
-        driver.findElement(By.xpath("//button[text()='Registration']")).click();
-        //assert Sign out button displayed - //button[contains(.,'Sign Out')] - xpath
-        Assert.assertTrue(isElementPresent2(By.xpath("//button[contains(.,'Sign Out')]")));
+        app.getUser().clickOnRegistrationButton();
+        //assert alert is appeared
+        Assert.assertTrue(app.getUser().isAlertPresent());
     }
+
 }
--- src/test/java/org/ait/phonebook/TestBase.java
+++ src/test/java/org/ait/phonebook/TestBase.java
@@ -1,45 +1,40 @@
 package org.ait.phonebook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
+import org.ait.phonebook.fw.ApplicationManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     WebDriver driver;
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp(){
-        driver = new ChromeDriver();
-        driver.get("https://telranedu.web.app/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        app.init();
 
     }
 
-    public  boolean isHomeComponentPresent(){
-        return driver.findElements(By.xpath("//h1[text()='Home Component']")).size()>0;
-    }
-
-    public  boolean isElementPresent(By locator){
-        return driver.findElements(locator).size()>0;
-    }
-
-    public  boolean isElementPresent2(By locator){
-        try{
-            driver.findElement(locator);
-            return  true;
-        }catch (NoSuchElementException ex){
-            return  false;
-        }
-    }
-
-    @AfterMethod (enabled = false)
+    @AfterSuite
     public void tearDown(){
-        driver.quit();
+        app.stop();
     }
+
+    @BeforeMethod
+    public void startTest(){
+        logger.info("Start test");
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
+    }
+
 }
